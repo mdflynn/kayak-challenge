7a853e44dbd8d31b49c8fea9bc3dac28db9280b8
Mike Flynn <mdflynn34@outlook.com>
2021-09-12 12:36:53 -0600

    update values

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,14 +1,19 @@
 import React from "react";
 
 const Filter = ({ filter }) => {
+
+    const handleFilterChange = (e) => {
+        console.log(e.target);
+
+    }
   return (
-    <form className="filter-form" onChange={filter}>
-      <input type="checkbox" name="oneworld" value="sa" />
-      <label for="oneworld">OneWorld</label>
-      <input type="checkbox" name="skyteam" value="sa" />
-      <label for="skyteam">Sky team</label>
-      <input type="checkbox" name="staralliance" value="sa" />
-      <label for="staralliance">Star Alliance</label>
+    <form className="filter-form" onChange={handleFilterChange}>
+      <input type="checkbox" name="oneworld" value="OW" />
+      <label htmlFor="oneworld">OneWorld</label>
+      <input type="checkbox" name="skyteam" value="ST" />
+      <label htmlFor="skyteam">Sky team</label>
+      <input type="checkbox" name="staralliance" value="SA" />
+      <label htmlFor="staralliance">Star Alliance</label>
     </form>
   );
 };
