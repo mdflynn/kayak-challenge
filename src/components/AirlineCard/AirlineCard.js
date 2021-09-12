cf5c337d5c480bf772167730fb4a16762bd14b7d
Mike Flynn <mdflynn34@outlook.com>
2021-09-12 14:28:07 -0600

    add state for class update on hover

--- src/components/AirlineCard/AirlineCard.js
+++ src/components/AirlineCard/AirlineCard.js
@@ -1,20 +1,26 @@
-import React from "react";
-import "./AirlineCard.scss"
+import React, { useState } from "react";
+import "./AirlineCard.scss";
 
 const AirlineCard = ({ airlineDetails }) => {
   const { alliance, logoURL, name, phone, site } = airlineDetails;
+  const [displayDetails, setDisplayDetails] = useState("hide-details");
+
   const imgSrc = "https://kayak.com" + logoURL;
 
   return (
-    <article className="card-container">
-      <div className="card-title-container">
-        <img className="card-img" src={imgSrc} alt={`${name} logo`} />
-        <p className="card-title">{name}</p>
-      </div>
+    <article
+      className="card-container"
+      onMouseEnter={() => setDisplayDetails("hover-details")}
+      onMouseLeave={() => setDisplayDetails("hide-details")}
+    >
+      <img className="card-img" src={imgSrc} alt={`${name} logo`} />
       <div className="airline-details">
-        <p className="airline-alliance">{alliance}</p>
-        <p className="airline-phone">{phone}</p>
-        <p className="airline-url">{site}</p>
+        <p className="airline-name">{name}</p>
+        <div className={displayDetails}>
+          <p className="airline-alliance">{alliance}</p>
+          <p className="airline-phone">{phone}</p>
+          <p className="airline-url">{site}</p>
+        </div>
       </div>
     </article>
   );
