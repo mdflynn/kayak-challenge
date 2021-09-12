ee118cf14f56ace33468a72769487f90c01a9ace
Mike Flynn <mdflynn34@outlook.com>
2021-09-12 11:00:42 -0600

    add img and classNames to Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./Nav.scss";
+
+import logo from "../../assets/kayak.svg";
 
 const Nav = () => {
-  return <h1>Kayak Challenge</h1>;
+  return (
+    <nav className="nav">
+      <img className="nav-logo" src={logo} alt="kayak logo" />
+    </nav>
+  );
 };
 
 export default Nav;
