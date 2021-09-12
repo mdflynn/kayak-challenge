b07ef609d84851aa5b68d21bfce1a26746e0d83a
Mike Flynn <mdflynn34@outlook.com>
2021-09-12 12:35:00 -0600

    add filter details

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Filter = () => {
-    return <p>Filter</p>
-}
+const Filter = ({ filter }) => {
+  return (
+    <form className="filter-form" onChange={filter}>
+      <input type="checkbox" name="oneworld" value="sa" />
+      <label for="oneworld">OneWorld</label>
+      <input type="checkbox" name="skyteam" value="sa" />
+      <label for="skyteam">Sky team</label>
+      <input type="checkbox" name="staralliance" value="sa" />
+      <label for="staralliance">Star Alliance</label>
+    </form>
+  );
+};
 
 export default Filter;
