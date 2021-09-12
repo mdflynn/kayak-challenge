b3a9b23b970e110b65680342bbb8d4278520698e
Mike Flynn <mdflynn34@outlook.com>
2021-09-12 11:07:54 -0600

    add rough draft jsx structure

--- src/components/AirlineCard/AirlineCard.js
+++ src/components/AirlineCard/AirlineCard.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const AirlineCard = ({ airlineDetails }) => {
-  return <h1>Airline</h1>;
+  return (
+    <article className="card-container">
+      <div className="card-title-container">
+        <img className="card-img" />
+        <p className="card-title">Title</p>
+      </div>
+      <div className="airline-details">
+        <p className="airline-alliance">Alliance</p>
+        <p className="airline-phone">Phone</p>
+        <p className="airline-url">Url</p>
+      </div>
+    </article>
+  );
 };
 
 export default AirlineCard;
