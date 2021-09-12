5e8a47f1e9c4b5171ea476abcd2942b7742c5833
Mike Flynn <mdflynn34@outlook.com>
2021-09-12 11:11:43 -0600

    destructure prop and add to jsx

--- src/components/AirlineCard/AirlineCard.js
+++ src/components/AirlineCard/AirlineCard.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 const AirlineCard = ({ airlineDetails }) => {
+  const { alliance, logoURL, name, phone, site } = airlineDetails;
+
   return (
     <article className="card-container">
       <div className="card-title-container">
-        <img className="card-img" />
-        <p className="card-title">Title</p>
+        <img className="card-img" src={logoURL} alt={`${name} logo`} />
+        <p className="card-title">{name}</p>
       </div>
       <div className="airline-details">
-        <p className="airline-alliance">Alliance</p>
-        <p className="airline-phone">Phone</p>
-        <p className="airline-url">Url</p>
+        <p className="airline-alliance">{alliance}</p>
+        <p className="airline-phone">{phone}</p>
+        <p className="airline-url">{site}</p>
       </div>
     </article>
   );
