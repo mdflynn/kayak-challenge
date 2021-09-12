db7414e4371f95bccfe967a8a39f5cfec0d93825
Mike Flynn <mdflynn34@outlook.com>
2021-09-12 12:56:50 -0600

    add func to generate Cards with filter

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -5,27 +5,40 @@ import { v4 as uuidv4 } from "uuid";
 
 import { fetchAirlineData } from "../../apiCalls";
 import AirlineCard from "../AirlineCard/AirlineCard";
+import Filter from "../Filter/Filter";
 
 const HomePage = () => {
   const [airlineData, setAirlineData] = useState([]);
+  const [filterOptions, setFilterOptions] = useState([]);
 
   useEffect(() => {
     fetchAirlineData(setAirlineData);
   }, []);
 
-  const generateAirlineCards = () => {
-    return airlineData.map((airline) => {
-
+  const generateAirlineCards = (data) => {
+    return data.map((airline) => {
       const cardKey = uuidv4();
 
       return <AirlineCard key={cardKey} airlineDetails={airline} />;
     });
   };
 
+  const updateFilteredAirlines = () => {
+    return airlineData.filter(airline => filterOptions.includes(airline.alliance))
+  }
+
+  const determineDisplay = filterOptions.length > 0 ? updateFilteredAirlines() : airlineData;
+
   return (
-    <section className="card-display">
-      {airlineData.length > 0 && generateAirlineCards()}
-    </section>
+    <main>
+      <p>Airlines</p>
+      <p>Filter By Alliances</p>
+      <Filter filter={setFilterOptions} />
+      {filterOptions.length > 0 && console.log(filterOptions)}
+      <section className="card-display">
+        {airlineData.length > 0 && generateAirlineCards(determineDisplay)}
+      </section>
+    </main>
   );
 };
 
