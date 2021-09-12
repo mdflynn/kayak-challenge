041a43a9090a7d03476159c57c5372b54b417ed4
Mike Flynn <mdflynn34@outlook.com>
2021-09-12 07:59:00 -0600

    update param name

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,13 @@
 import jsonp from "jsonp";
 
-export const fetchAirlineData = async (dataContainer) => {
+export const fetchAirlineData = async (setStateFunc) => {
   const url = "https://kayak.com/h/mobileapis/directory/airlines/homework";
 
   const response = await jsonp(url, { param: "jsonp" }, (err, data) => {
     if (err) {
       console.error(err.message);
     } else {
-      dataContainer(data);
+      setStateFunc(data);
     }
   });
 
