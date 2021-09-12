7148402e795c8e8e585c415761cf478f19cd9d5d
Mike Flynn <mdflynn34@outlook.com>
2021-09-12 15:32:37 -0600

    remove app.scss file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
 
-import HomePage from '../HomePage/HomePage';
-import Nav from '../Nav/Nav';
+import HomePage from "../HomePage/HomePage";
+import Nav from "../Nav/Nav";
 
 const App = () => {
   return (
-   <>
-   <Nav />
-   <HomePage />
-   </>
+    <>
+      <Nav />
+      <HomePage />
+    </>
   );
-}
+};
 
 export default App;
